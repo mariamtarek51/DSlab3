5515f7a57832252ed59bf71922485aaaf215b56a
norah <11Norah>
2022-03-25 11:34:37 +0200

    max heap

--- maxHeap.java
+++ maxHeap.java
@@ -11,14 +11,17 @@ public class maxHeap {
 	}
 	
 	void heapify(ArrayList<Integer> array,int size,int i) {
-		int l=2*i+1;
-		int r=2*i+2;
+		int l=2*i+1; //left child
+		int r=2*i+2; //right child
 		int max=i;
+		//checking heap property
 		if(l<size && array.get(l)>array.get(max)) {
 		max=l;}
 		if(r<size && array.get(r)>array.get(max)) {
 		max=r;
              }
+		//if not the same ,they are swapped and continue checking
+		//until the max is in the root
 		if(max != i){
 	   Collections.swap(array, i, max);
 		
@@ -37,6 +40,34 @@ public class maxHeap {
 			heapify(array,i,0);
 		}
 	}
+///-------------------TO IMPLEMENT PRIORITY QUEUE---------------
+	int Extract_Max(ArrayList<Integer> array){
+		//in the binary heap ,the max is the root
+		int maximum=array.get(0);
+		int n=array.size();
+		//putting the last leaf in the place of root
+		array.set(0, array.get(n-1));
+		n=n-1;
+        heapify(array,n,0);
+        return maximum;
+	}
+	ArrayList<Integer> MAX_HEAP=new ArrayList<>();
+	 int size=0;
+	void Heap_Max_Insert(int i){
+		//int size=-1;
+		//for(int i=0 ;i< array.size()-1;i++){
+		     MAX_HEAP.add(i);
+			 //shifting up to
+			//maintain the greatest element in the root
+		    int current =size;
+			int parent=(current-1)/2;
+
+			while(size>0 && MAX_HEAP.get(parent)<MAX_HEAP.get(current)){
+				Collections.swap(MAX_HEAP,parent,current);
+				current=parent;
+			}
+		size=size+1;
+	}
 	static void printArray(ArrayList<Integer> arr)
 	{
 		int n = arr.size();
@@ -49,17 +80,21 @@ public class maxHeap {
 	
 	
 		int num;
-		
+		maxHeap op=new maxHeap();
 		Scanner sc=new Scanner(System.in);
 		ArrayList<Integer> array = new ArrayList<Integer>();
-		array.add(5);
-		array.add(6);
-		array.add(9);
-		array.add(1);
-		printArray(array);
-		maxHeap op=new maxHeap();
+		op.Heap_Max_Insert(5);
+		op.Heap_Max_Insert(6);
+		op.Heap_Max_Insert(9);
+		op.Heap_Max_Insert(1);
+		printArray(op.MAX_HEAP);
+
 		op.heapsort(array);
         printArray(array);
+		array=op.MAX_HEAP;
+		System.out.println("the priority queue: ");
+		printArray(array);
+		System.out.println("the max heap: "+ op.Extract_Max(array));
 	/*
 		int n=array.size();*/
 	
