4d58cc9c06a28697efa25553e93a1144bf2e3626
11Norah <95590883+11Norah@users.noreply.github.com>
2022-04-01 10:11:17 +0200

    Update Merge_sort.java

--- Merge_sort.java
+++ Merge_sort.java
@@ -1,65 +1,146 @@
 package dslab3;
-
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.*;
+public class maxHeap {
+	void buildheap(ArrayList<Integer> array,int size) {
 
-public class Merge_sort {
-    void merge(ArrayList<Integer> array, int start, int middle, int end)
-    {
-        // Find sizes of two subarrays to be merged
-        int n1 = middle - start + 1;
-        int n2 = end - middle;
+		for (int i = size/ 2 - 1; i >= 0; i--)
+			heapify(array, size,i);
+	}
 
-        // Create two arrays
-        ArrayList<Integer> array1=new ArrayList<>(n1);
-        ArrayList<Integer> array2=new ArrayList<>(n2);
+	void heapify(ArrayList<Integer> array,int size,int i) {
+		int l=2*i+1; //left child
+		int r=2*i+2; //right child
+		int max=i;
+		//checking heap property
+		if(l<size && array.get(l)>array.get(max)) {
+			max=l;}
+		if(r<size && array.get(r)>array.get(max)) {
+			max=r;
+		}
+		//if not the same ,they are swapped and continue checking
+		//until the max is in the root
+		if(max != i){
+			Collections.swap(array, i, max);
 
-        /*Copy data to temp arrays*/
-        for (int i = 0; i < n1; i++)
-            array1.add(i,array.get(start+i));
-        for (int j = 0; j < n2; j++)
-            array2.add(j,array.get(middle+1+j));
-        /* Merge the temp arrays */
-        int i = 0, j = 0;
+			heapify(array,size,max);
+		}
 
-        // Initial index of merged subarray array
-        int k = start;
-        while (i < n1 && j < n2) {
-            if (array1.get(i) <= array2.get(j)) {
-                array.set(k,array1.get(i));
-                i++;
-            }
-            else {
-                array.set(k,array2.get(j));
-                j++;
-            }
-            k++;
-        }
+	}
+	void heapsort(ArrayList<Integer> array){
+		int n=array.size();
+		buildheap(array,n);
 
-       //copy the rest of the elements in the subarrays
-        while (i < n1) {
-            array.set(k,array1.get(i));
-            i++;
-            k++;
-        }
-        while (j < n2) {
-            array.set(k,array2.get(j));
-            j++;
-            k++;
-        }
-    }
-    ArrayList<Integer> sort(ArrayList<Integer> arr, int l, int r)
-    {
-        if (l < r) {
-            // Find the middle point
-            int middle =l+ (r-l)/2;
+		for (int i=n-1; i>=0;i--){
+			int temp=array.get(0);
+			array.set(0,array.get(i));
+			array.set(i,temp);
+			heapify(array,i,0);
+		}
+	}
+	void bubblesort(ArrayList<Integer> array){
+		int n = array.size();
+		for (int i = 0; i < n-1; i++)
+			for (int j = 0; j < n-i-1; j++)
+				if (array.get(j) > array.get(j+1))
+				{
+					// swap arr[j+1] and arr[j]
+					int temp = array.get(j);
+					array.set(j,array.get(j+1));
+					array.set(j+1,temp) ;
+				}
 
-            // Sort first and second halves
-            sort(arr, l, middle);
-            sort(arr, middle + 1, r);
 
-            // Merge them
-            merge(arr, l, middle, r);
 
-        } return arr;
-    }
+
+
+	}
+	///-------------------TO IMPLEMENT PRIORITY QUEUE---------------
+	int Extract_Max(ArrayList<Integer> array){
+		//in the binary heap ,the max is the root
+		int maximum=array.get(0);
+		int n=array.size();
+		//putting the last leaf in the place of root
+		array.set(0, array.get(n-1));
+		n=n-1;
+		heapify(array,n,0);
+		return maximum;
+	}
+
+	int size=0;
+	void Heap_Max_Insert(ArrayList<Integer> MAX_HEAP, int i){
+
+		MAX_HEAP.add(i);
+		//shifting up to
+		//maintain the greatest element in the root
+		int current =size;
+		int parent=(current-1)/2;
+
+		while(size>0 && MAX_HEAP.get(parent)<MAX_HEAP.get(current)){
+			Collections.swap(MAX_HEAP,parent,current);
+			current=parent;
+		}
+		size=size+1;
+	}
+	void printArray(ArrayList<Integer> arr)
+	{
+		int n = arr.size();
+		for (int i = 0; i < n; ++i)
+			System.out.print(arr.get(i) + " ");
+		System.out.println();
+	}
+
+	public static void main(String[] args) {
+
+		int num;
+		maxHeap op=new maxHeap();
+		Scanner sc=new Scanner(System.in);
+		ArrayList<Integer> array = new ArrayList<Integer>();
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		System.out.println("Enter number of elements: ");
+		int n=sc.nextInt();
+		System.out.println("Enter the elements of the array: ");
+		for(int i=0; i<n; i++)
+		{
+//reading array elements from the user
+			op.Heap_Max_Insert(array,sc.nextInt());
+		}
+
+		System.out.println("the priority queue: ");
+		op.printArray(array);
+		do {
+			System.out.println("Enter 1 to extract max\n" +
+					"2 to sort the elements by heapsort \n" +
+					"3 to sort elements by bubblesort :\n" +
+					"4 to sort elements by Mergesort :\n");
+			int y = sc.nextInt();
+			if (y == 1) {
+				//if you want extract max
+				for (int j=0;j<n;j++){arr.add(j,array.get(j));}
+				System.out.println("the max heap: " + op.Extract_Max(arr));
+			} else if (y == 2) {
+				//if you want to sort
+				for (int j=0;j<n;j++){arr.add(j,array.get(j));}
+				System.out.println("Sorted array: ");
+				op.heapsort(arr);
+				op.printArray(arr);
+			}
+			else if(y==3){
+				for (int j=0;j<n;j++){arr.add(j,array.get(j));}
+				System.out.println("Sorted array using bubble sort : ");
+				op.bubblesort(arr);
+				op.printArray(arr);
+
+
+			}
+			else if(y==4){
+				for (int j=0;j<n;j++){arr.add(j,array.get(j));}
+				System.out.println("Sorted array using merge sort : ");
+				Merge_sort j=new Merge_sort();
+				j.sort(arr,arr.get(0),arr.get(arr.size()-1));
+				op.printArray(j.sort(arr,0,arr.size()-1));}
+		}while(true);
+
+	}
 }
