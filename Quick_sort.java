ae941ca11f089941bfa01310a7afc948b8b9010a
11Norah <95590883+11Norah@users.noreply.github.com>
2022-04-02 12:21:20 +0200

    Update Quick_sort.java

--- Quick_sort.java
+++ Quick_sort.java
@@ -4,20 +4,13 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Quick_sort {
-    /* This function takes last element as pivot, places
-       the pivot element at its correct position in sorted
-       array, and places all smaller (smaller than pivot)
-       to left of pivot and all greater elements to right
-       of pivot */
+
      int partition(ArrayList<Integer> arr, int low, int high)
     {
-
-        // pivot
         int pivot = arr.get(high);
 
-        // Index of smaller element and
-        // indicates the right position
-        // of pivot found so far
+        // Index of smaller element
+        // assumed  the right position
         int i = (low - 1);
 
         for(int j = low; j <= high - 1; j++)
@@ -34,24 +27,17 @@ public class Quick_sort {
         return (i + 1);
     }
 
-    /* The main function that implements QuickSort
-              arr[] --> Array to be sorted,
-              low --> Starting index,
-              high --> Ending index
-     */
     void quickSort(ArrayList<Integer> arr, int low, int high)
     {
         if (low < high)
         {
 
-            // pi is partitioning index, arr[p]
+            // pivot is partitioning index, arr[p]
             // is now at right place
-            int pi = partition(arr, low, high);
+            int pivot = partition(arr, low, high);
 
-            // Separately sort elements before
-            // partition and after partition
-            quickSort(arr, low, pi - 1);
-            quickSort(arr, pi + 1, high);
+            quickSort(arr, low, pivot - 1);
+            quickSort(arr, pivot + 1, high);
         }
     }
 
